b393c17b4ca71a4f7e53d7bb8bb85c22d4c0d7cf
Mircea N <nunu.mircea@gmail.com>
2023-03-23 02:20:23 -0400

    redux exercice
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
+import {createStore, configureStore} from 'redux';
+import {configureStore} from '@reduxjs/toolkit'
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
 
 
 //Create the store
-const myStore = createStore(myReducers);
+const myStore = configureStore(myReducers);
 
 //This will console log the current state everytime the state changes
 myStore.subscribe(()=>console.log(myStore.getState()));
